87e2109395fe777e0dcf370503014d1fa55c7d1b
iyke quame <nanaquameallday@gmail.com>
2017-05-31 13:37:08 +0000

    fix error on screen density
    PRODUCT_AAPT_PREF_CONFIG := hdpi
    PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi 320dpi hdpi tvdpi mdpi ldpi

--- device_Camon_C7.mk
+++ device_Camon_C7.mk
@@ -16,8 +16,8 @@
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
-PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi 480dpi hdpi tvdpi mdpi ldpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi 320dpi hdpi tvdpi mdpi ldpi
 
 # Device specific overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
@@ -28,10 +28,10 @@ TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 include $(LOCAL_PATH)/product/*.mk
 
 # Dalvik heap configurations
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
+$(call inherit-product-if-exists, frameworks/native/build/phone-hdpi-3072-dalvik-heap.mk)
 
 # Call hwui memory config
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
+$(call inherit-product-if-exists, frameworks/native/build/phone-hdpi-3072-hwui-memory.mk)
 
 # Product common configurations
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
